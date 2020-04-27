1c566f7e86da1655c7dc71576d956f172b392582
Brian <mail@standinshd.site>
2020-04-27 18:13:37 +0700

    glitch ping robot

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const path = require("path");
 const app = express();
+const http = require('http');
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "hbs");
@@ -10,8 +11,13 @@ app.use("/assets", express.static("assets"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 250000);
+
 app.get("/", (req, res) => {
-  res.render("index");
+  console.log(Date.now() + " Ping Received");
+  res.status(200).render("index");
 });
 
-app.listen(3000, console.log("Listening on port 3000."));
+app.listen(3000, console.log("Listening on port 3000!"));
